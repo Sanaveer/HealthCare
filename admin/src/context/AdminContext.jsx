532c0fd06c084b38e0c5063187e5e639d7fd625b
Sanaveer <sanaalhan2822@gmail.com>
2025-04-25 02:14:50 +0530

    Update AdminContext.jsx

--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -12,7 +12,7 @@ const AdminContextProvider = (props) => {
   const [appointments, setAppointments] = useState([])
   const [dashData,setDashData] = useState(false) 
 
-  const backendUrl = import.meta.env.VITE_BACKEND_URL;
+  const backendUrl = "https://healthcare-1-96gz.onrender.com"
 
   const getAllDoctors = async () => {
     try {
